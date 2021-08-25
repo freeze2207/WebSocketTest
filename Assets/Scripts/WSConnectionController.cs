c572d5d00670c3abdab5607c45760670a40481ec
Zihe Zhang <zhangzihezealf@gmail.com>
2021-08-25 11:37:22 -0400

    Initial commit

---
+++ Assets/Scripts/WSConnectionController.cs
@@ -0,0 +1,101 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using WebSocketSharp;
+
+public class WSConnectionController : MonoBehaviour
+{
+    [System.Serializable]
+    private class CYAPlayerInfo
+    {
+        public string role;
+        public string spotID;
+        public string username;
+        public string playerID;
+        public HashSet<string> commandHandlers;
+        public string token;
+    }
+
+    [System.Serializable]
+    private class Payload
+    {
+        string[] group;
+    }
+
+    [System.Serializable]
+    private class TokenResult
+    {
+        public Payload payload;
+        public string token;
+        public string auth;        
+    }
+
+
+    private const string HttpURL = "https://open.cyalive.co";
+    private const string SocketURL = "wss://opensocket.cyalive.co";
+
+    private WebSocket ws = null;
+    private string WebSocketConnectionToken = string.Empty;
+    private CYAPlayerInfo cyaPlayer = new CYAPlayerInfo();
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        string JSONString = JsonUtility.ToJson(new TokenResult());
+        StartCoroutine(PostRequest(HttpURL + "/v2/gen", JSONString));
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (ws == null)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ws.Send("Hello");
+        }
+    }
+
+    IEnumerator PostRequest(string url, string json)
+    {
+        UnityWebRequest unityWebRequest = new UnityWebRequest(url, UnityWebRequest.kHttpVerbPOST);
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        unityWebRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        unityWebRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        unityWebRequest.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        unityWebRequest.SetRequestHeader("Authorization", "1");
+
+        //Send the request then wait here until it return
+        yield return unityWebRequest.SendWebRequest();
+
+        if (unityWebRequest.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log("Error While Sending: " + unityWebRequest.error);
+        }
+        else
+        {
+            TokenResult result = JsonUtility.FromJson<TokenResult>(unityWebRequest.downloadHandler.text);
+            this.cyaPlayer.token = "bearer " + result.auth;
+            this.WebSocketConnectionToken = result.token;
+            //Debug.Log(result.token);
+
+            ws = new WebSocket(SocketURL + "/ws?token=" + this.WebSocketConnectionToken);
+            
+            ws.OnOpen += (sender, e) =>
+            {
+                Debug.Log("Socket opened");
+            };
+            ws.OnMessage += (sender, e) =>
+            {
+                Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+            };
+
+            ws.Connect();
+
+        }
+    }
+}
